1e52caffd9c97e008db6766dc47cdaf5259338f9
Rajarshi Chakraborty <rajarshichakraborty2005@gmail.com>
2026-09-17 00:50:27 +0530

    chore: add perfect dependencies

--- Server/schemas/user.schema.ts
+++ Server/schemas/user.schema.ts
@@ -1,10 +1,17 @@
-/* Dummy schema for testing */
+import { pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+import { z } from "zod";
 
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+export const userTable = pgTable("user", {
+  username: varchar("username", { length: 255 }).primaryKey(),
+  email: varchar("email", { length: 255 }).unique(),
+  hashedPassword: varchar("hashedPassword", { length: 255 }).notNull(),
+  fullName: varchar("fullName", { length: 255 }).notNull(),
+  bio: varchar("bio", { length: 255 }).notNull(),
+  profileImageUrl:varchar("profileImageUrl", { length: 255 }).notNull(),
+  phoneNumber: varchar("phoneNumber", { length: 255 }).notNull(),
+  createdAt: timestamp("createdAt", { withTimezone: true }).defaultNow().notNull(),
+  updatedAt: timestamp("updatedAt", { withTimezone: true }).defaultNow().notNull(),
+})
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
+
+export type UserTable = z.infer<typeof userTable>;
