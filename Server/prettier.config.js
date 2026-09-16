49afbee565dc6ce146d6c082c2d83cc842a3fed9
Rajarshi Chakraborty <rajarshichakraborty2005@gmail.com>
2026-09-16 20:55:36 +0530

    chore: add perfect folder structure

---
+++ Server/prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
